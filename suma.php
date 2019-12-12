9f2490f05f3d2f5709746d208cd90029b3a249d7
Marlon <you@example.com>
2019-12-12 08:55:51 -0500

    rev 40

--- suma.php
+++ suma.php
@@ -3,7 +3,7 @@
   $total = $_REQUEST['numero1']+$_REQUEST['numero2'];
    echo "esto es una prueba";
     echo "esto es una prueba2";
-
+echo "esto es una prueba de la rama2";
 
  echo "El total de la suma es: ".$total;
     
